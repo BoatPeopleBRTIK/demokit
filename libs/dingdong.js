4c876a172780bc9c76ed2d96788895c38f9d74d7
Inho Oh <webispy@gmail.com>
2016-11-22 01:17:27 -0500

    mplayer: Fix bug for mplayer child process remain issue

--- libs/dingdong.js
+++ libs/dingdong.js
@@ -18,12 +18,13 @@
 const rtspPlayer = require('./playrtsp')
 const MPlayer = require('mplayer')
 
+const player = new MPlayer()
+player.on('start', console.log.bind(this, 'playback started'))
+player.on('stop', console.log.bind(this, 'playback stoped'))
+player.on('status', console.log)
+
 module.exports.setTrigger = function (src) {
   src.on('on', () => {
-    const player = new MPlayer()
-    player.on('start', console.log.bind(this, 'playback started'))
-    player.on('stop', console.log.bind(this, 'playback stoped'))
-    player.on('status', console.log)
     player.openFile('./data/ding_dong_bell_door.mp3')
 
     rtspPlayer.toggle()
