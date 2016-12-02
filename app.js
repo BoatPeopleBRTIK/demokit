0618cda47898f01022a0c8f8d8a97ffa91a4fe88
Inho Oh <webispy@gmail.com>
2016-12-01 21:23:39 -0500

    www: Add reboot button

--- app.js
+++ app.js
@@ -80,6 +80,16 @@ app.post('/info', (req, res) => {
   res.redirect('/setup')
 })
 
+app.get('/reboot/master', (req, res) => {
+  res.end()
+  settings.reboot()
+})
+
+app.get('/reboot/slave', (req, res) => {
+  ipc.sendReboot()
+  res.end()
+})
+
 app.get('/', function (req, res, next) {
   res.render('index', {
     demokit: settings,
